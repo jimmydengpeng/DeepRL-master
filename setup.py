680b3f25d46176b76a829c7f15ce43c92ed6410b
JimmyDeng <121dengpeng@163.com>
2023-02-21 21:42:03 +0800

    init

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import sys
+
+print('Please install OpenAI Baselines (commit 8e56dd) and requirement.txt')
+
+# if not (sys.version.startswith('3.5') or sys.version.startswith('3.6')):
+#     raise Exception('Only Python 3.5 and 3.6 are supported')
+
+if sys.version_info.major != 3:
+    print('This Python is only compatible with Python 3, but you are running '
+          'Python {}. The installation will likely fail.'.format(sys.version_info.major))
+
+
+setup(name='deep_rl',
+      packages=[package for package in find_packages()
+                if package.startswith('deep_rl')],
+      install_requires=[],
+      description="Modularized Implementation of Deep RL Algorithms",
+      author="Shangtong Zhang",
+      url='https://github.com/ShangtongZhang/DeepRL',
+      author_email="zhangshangtong.cpp@gmail.com",
+      version="1.5")
